86246b42db143b4b285ea0bef918e36a211b71f1
l-yazan-l <103342472+l-yazan-l@users.noreply.github.com>
2023-08-11 19:27:20 +0300

    Update sign-up.php

--- sign-up/sign-up.php
+++ sign-up/sign-up.php
@@ -1,57 +1,60 @@
-<?php
 
-if (empty($_POST["name"])) {
+<?php
+if(empty($_POST["name"]))
+{
     die("Name is required");
 }
-
-if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))
+{
     die("Valid email is required");
 }
-
-if (strlen($_POST["password"]) < 8) {
+if(strlen($_POST['password']) < 8)
+{
     die("Password must be at least 8 characters");
 }
-
-if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
-    die("Password must contain at least one letter");
+if(!preg_match("/[a-z]/i" , $_POST["password"]))
+{
+    die("It must contain at least 1 letter");
 }
-
-if ( ! preg_match("/[0-9]/", $_POST["password"])) {
-    die("Password must contain at least one number");
+if(!preg_match("/[0-9]/i" , $_POST["password"]))
+{
+    die("It must contain at least 1 number");
 }
-
-if ($_POST["password"] !== $_POST["password-confirmation"]) {
-    die("Passwords must match");
+if($_POST['password'] !== $_POST['password-confirmation'])
+{
+    die("Password Dose not match");
 }
 
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-$mysqli = require __DIR__ . "/database.php";
+$mysqli = require __DIR__."/database.php";
 
 $sql = "INSERT INTO user (name, email, password_hash)
-        VALUES (?, ?, ?)";
+        VALUES (? , ? , ?)";
+$stmt = $mysqli -> stmt_init();
 
-$stmt = $mysqli->stmt_init();
-
-if ( ! $stmt->prepare($sql)) {
-    die("SQL error: " . $mysqli->error);
+if(! $stmt -> prepare($sql))
+{
+    die ("SQL error : " . $mysqli -> error);
 }
+$stmt -> bind_param("sss",
+                    $_POST['name'],
+                    $_POST['email'],
+                    $password_hash);
 
-$stmt->bind_param("sss",
-                  $_POST["name"],
-                  $_POST["email"],
-                  $password_hash);
-
-if ($stmt->execute()) {
-
-    header("Location: signup-success.php");
-    exit;
-
-} else {
-
-    if ($mysqli->errno === 1062) {
-        die("email already taken");
-    } else {
-        die($mysqli->error . " " . $mysqli->errno);
+if ($stmt -> execute())
+{
+    echo "Sign-Up Successful";
+}
+    else
+    {
+        if($mysqli->errno === 1062)
+        {
+            die ("Email Already Taken");
+        }
+    
+    else
+    {
+        die($mysqli -> error. " ". $mysqli->errno);
     }
 }
