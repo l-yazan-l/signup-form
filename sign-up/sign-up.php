3285cc04315c860b7c6a4521c42d15d23fab39d6
l-yazan-l <103342472+l-yazan-l@users.noreply.github.com>
2023-08-11 19:31:57 +0300

    Update sign-up.php

--- sign-up/sign-up.php
+++ sign-up/sign-up.php
@@ -1,60 +1,50 @@
-
 <?php
-if(empty($_POST["name"]))
-{
+if (empty($_POST["name"])) {
     die("Name is required");
 }
-if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))
-{
+
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
     die("Valid email is required");
 }
-if(strlen($_POST['password']) < 8)
-{
+
+if (strlen($_POST['password']) < 8) {
     die("Password must be at least 8 characters");
 }
-if(!preg_match("/[a-z]/i" , $_POST["password"]))
-{
+
+if (!preg_match("/[a-z]/i", $_POST["password"])) {
     die("It must contain at least 1 letter");
 }
-if(!preg_match("/[0-9]/i" , $_POST["password"]))
-{
+
+if (!preg_match("/[0-9]/i", $_POST["password"])) {
     die("It must contain at least 1 number");
 }
-if($_POST['password'] !== $_POST['password-confirmation'])
-{
-    die("Password Dose not match");
+
+if ($_POST['password'] !== $_POST['password-confirmation']) {
+    die("Password does not match");
 }
 
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $mysqli = require __DIR__."/database.php";
 
-$sql = "INSERT INTO user (name, email, password_hash)
-        VALUES (? , ? , ?)";
-$stmt = $mysqli -> stmt_init();
+// Check if the email already exists
+$checkEmailQuery = "SELECT id FROM user WHERE email = ?";
+$checkEmailStmt = $mysqli->prepare($checkEmailQuery);
+$checkEmailStmt->bind_param("s", $_POST['email']);
+$checkEmailStmt->execute();
+$checkEmailResult = $checkEmailStmt->get_result();
 
-if(! $stmt -> prepare($sql))
-{
-    die ("SQL error : " . $mysqli -> error);
+if ($checkEmailResult->num_rows > 0) {
+    die("Email Already Taken");
 }
-$stmt -> bind_param("sss",
-                    $_POST['name'],
-                    $_POST['email'],
-                    $password_hash);
 
-if ($stmt -> execute())
-{
+// If email is not taken, proceed with insertion
+$insertQuery = "INSERT INTO user (name, email, password_hash) VALUES (?, ?, ?)";
+$insertStmt = $mysqli->prepare($insertQuery);
+$insertStmt->bind_param("sss", $_POST['name'], $_POST['email'], $password_hash);
+
+if ($insertStmt->execute()) {
     echo "Sign-Up Successful";
-}
-    else
-    {
-        if($mysqli->errno === 1062)
-        {
-            die ("Email Already Taken");
-        }
-    
-    else
-    {
-        die($mysqli -> error. " ". $mysqli->errno);
-    }
+} else {
+    die($insertStmt->error . " " . $mysqli->errno);
 }
